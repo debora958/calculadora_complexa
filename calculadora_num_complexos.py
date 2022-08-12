d32b39ba67f9c6d89fa76e8614d04a75134cdc87
debora958 <109834177+debora958@users.noreply.github.com>
2022-08-12 18:54:03 -0300

    Add files via upload

---
+++ calculadora_num_complexos.py
@@ -0,0 +1,27 @@
+class calculadora_num_complexos:
+    def __init__(self, numero_1, numero_2, numero_3):
+        self.numero_1 = numero_1 
+        self.numero_2 = numero_2
+        self.numero_3 = numero_3
+
+    def somar(self):
+        resultado = (self.numero_1 + self.numero_2) + self.numero_3
+        print(resultado)
+
+    def subtrair(self):
+        resultado = (self.numero_1 - self.numero_2) - self.numero_3
+        print(resultado)
+
+    def dividir(self):
+        resultado = (self.numero_1 / self.numero_2) / self.numero_3
+
+    def multiplicar(self):
+        resultado = (self.numero_1 * self.numero_2) * self.numero_3
+
+
+
+
+
+
+
+
